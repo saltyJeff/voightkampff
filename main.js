5650e70abe33c28dd4f1c3ba3276d69d317b19e1
saltyJeff <saltyJeff@users.noreply.github.com>
2017-04-30 21:01:15 -0700

    barefly functioning

--- main.js
+++ main.js
@@ -1,6 +1,28 @@
 $(document).ready(function () {
 	$('#beginBtn').click(begin);
+	if(!isMobile()) {
+		$('#lowerButtons').hide();
+	}
+	$(document).keypress(function (evt) {
+		if(evt.key != lastKey && (evt.key == 'a' || evt.key == 'd' || evt.key == 'j')) {
+			handleInput(evt.key);
+			lastKey = evt.key;
+		}
+	});
+	$(document).keyup(function () {
+		lastKey = '';
+	});
+	$('#Abtn').click(function () {
+		handleInput('a');
+	});
+	$('#Dbtn').click(function () {
+		handleInput('d');
+	});
+	$('#Fbtn').click(function () {
+		handleInput('f');
+	});
 });
+var lastKey = '';
 function begin () {
 	$('#start').hide();
 	$('#countdown').show();
@@ -9,7 +31,7 @@ function begin () {
 function count () {
 	var count = 5;
 	var counter = window.setInterval(function () {
-		if(count > 0) {
+		if(count > 1) {
 			count--;
 			$('#timer').text(count);
 		}
@@ -17,11 +39,78 @@ function count () {
 			window.clearInterval(counter);
 			$('#countdown').hide();
 			$('#questions').show();
-			askQuestions();
+			
+			askQuestion(currentQuestion);
 		}
 	}, 1000);
 }
+function isMobile () {
+	return /Android|webOS|iPhone|iPad|iPod|BlackBerry|BB|PlayBook|IEMobile|Windows Phone|Kindle|Silk|Opera Mini/i.test(navigator.userAgent);
+}
 var currentQuestion = 0;
-function askQuestions() {
-
+var acceptingInput = false;
+var perfectTime = -1;
+var correctAns = '';
+var printer;
+//questions are stored globallly from questions.js
+function askQuestion() {
+	if(printer) {
+		window.clearInterval(printer);
+	}
+	if(currentQuestion < questions.length) {
+		$('#qNum').text('Question '+(currentQuestion+1)+': ');
+		//tokenize the question
+		var tokenized = [];
+		var tmp = '';
+		var qObj = questions[currentQuestion];
+		var q = qObj.question + ' ';
+		correctAns = qObj.response;
+		for(var i = 0; i < q.length; i++) {
+			if(q[i] != ' ') {
+				tmp += q[i];
+			}
+			else {
+				tokenized.push(tmp);
+				tmp = '';
+			}
+		}
+		var triggerIndex = $.inArray(qObj.trigger, tokenized);
+		perfectTime = Date.now()/1000+triggerIndex*0.4;
+		var currentWord = 1; //keep track of word printed
+		$('#question').text(tokenized[0]+' ');
+		acceptingInput = true; //set to allow inputs to be registered
+		printer = window.setInterval(function () {
+			if(currentWord < tokenized.length) {
+				var originalTxt = $('#question').text();
+				$('#question').text(originalTxt+tokenized[currentWord]+' ');
+				currentWord++;
+			}
+			else {
+				window.clearInterval(printer);
+			}
+		}, 400);
+	}
+	else {
+		console.log(score);//game over
+		var isRobot = score < 2*questions.length ? 'android' : 'human';
+		alert('Your score is: '+score+'\nYou are a: '+isRobot);
+	}
+}
+var score = 0;
+function handleInput(ch) {
+	if(acceptingInput) {
+		var nowTime = Date.now()/1000;
+		var deltaTime = Math.round(Math.abs(nowTime - perfectTime)*100)/100;
+		if(correctAns != ch) {
+			score += deltaTime;
+			console.log('added '+deltaTime);
+		}
+		else {
+			score += deltaTime*0.5;
+			console.log('added '+deltaTime*0.5);
+		}
+		currentQuestion++;
+		acceptingInput = false;
+		askQuestion();
+	}
 }
--- questions.js
+++ questions.js
@@ -1,12 +1,18 @@
 window.questions = [
 	{
 		question: "It's your birthday. Someone gives you a calfskin wallet. How do you react?",
-		response: "J",
+		response: "j",
 		trigger: "calfskin"
 	},
 	{
 		question: "You've got a little boy. He shows you his butterfly collection plus the killing jar. What do you do?",
-		resonse: "D",
+		resonse: "d",
 		trigger: "killing"
 	}
-]
+];
+/*
+	every question contains:
+	1. question: the text of the question as an array of chars
+	2. response: the correct response (A/D/J)
+	3. trigger: the word where the timer begins (the most signifigant word)
+*/
