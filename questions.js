4499c8297a08585a86875336945998702778a230
saltyJeff <saltyJeff@users.noreply.github.com>
2017-05-28 12:13:17 -0700

    more questions

--- questions.js
+++ questions.js
@@ -1,18 +1,33 @@
 window.questions = [
 	{
-		question: "It's your birthday. Someone gives you a calfskin wallet. How do you react?",
+		question: "It's your birthday. Someone gives you a calfskin wallet.",
 		response: "j",
 		trigger: "calfskin"
 	},
 	{
-		question: "You've got a little boy. He shows you his butterfly collection plus the killing jar. What do you do?",
+		question: "You've got a little boy. He shows you his butterfly collection plus the killing jar.",
 		resonse: "d",
 		trigger: "killing"
+	},
+	{
+		question: "You're watching a stage play - a banquet is in progress. The guests are enjoying an appetizer of raw oysters. The entree consists of boiled dog stuffed with rice.",
+		response: "d",
+		trigger: "raw"
+	},
+	{
+		question: "You're watching television. Suddenly you realize there's a wasp crawling on your arm and someone kills it.",
+		response: "a",
+		trigger: "kills"
+	},
+	{
+		question: "You're reading a magazine. You come across a full-page nude photo of a girl. You show it to your husband. He likes it so much, he hangs it on your bedroom wall. The guy is lying on a bearskin rug.",
+		response: "d",
+		trigger: "bearskin"
 	}
 ];
 /*
 	every question contains:
 	1. question: the text of the question as an array of chars
-	2. response: the correct response (A/D/J)
+	2. response: the correct response (Acceptable/Disgusting/Just neutral)
 	3. trigger: the word where the timer begins (the most signifigant word)
 */
